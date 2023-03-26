99e9acc64b925ea846ccddb725c430c006946564
Name <email@example.ru>
2023-03-26 19:15:10 +0300

    add sidebar. footer and testing

---
+++ LifeSpot/Static/JavaScript/index.js
@@ -0,0 +1,10 @@
+﻿let yers = prompt("Сколько вам лет?", "")
+
+if (yers >= 18) {
+
+    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+}
+else {
+    alert("Наш сайт только для посещения совершеннолетними!")
+    window.location.href = "http://www.google.com"
+}
